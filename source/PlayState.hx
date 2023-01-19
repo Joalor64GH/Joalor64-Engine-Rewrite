9fa981bf0fb601d0c532a5129d5e83c8389834a1
Joalor64 <rhgarcon@gmail.com>
2023-01-19 14:41:06 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -105,7 +105,7 @@ import horny.*;
 
 using StringTools;
 
-class PlayState extends MusicBeatState implements IHook
+class PlayState extends MusicBeatState
 {
 	public static var STRUM_X = 42;
 	public static var STRUM_X_MIDDLESCROLL = -278;
