3eaf64d5c6ef2a0da2642a3ab27eb7ec1b60565e
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-02-05 22:06:22 -0500

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -30,9 +30,8 @@ class HealthIcon extends FlxSprite
 			setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
 	}
 
-	public function swapOldIcon() {
-		if(isOldIcon = !isOldIcon) changeIcon('bf-old');
-		else changeIcon('bf');
+	inline public function swapOldIcon() {
+		(!isOldIcon) ? changeIcon('bf-old') : changeIcon('bf');
 	}
 
 	private var iconOffsets:Array<Float> = [0, 0];
@@ -46,6 +45,7 @@ class HealthIcon extends FlxSprite
 			loadGraphic(file); //Load stupidly first for getting the file size
 			this.widthThing = width;
 			switch(width) {
+				// 1 icon
 				case 150:
 					loadGraphic(file, true, Math.floor(width), Math.floor(height));
 					iconOffsets[0] = (width - 150);
@@ -54,6 +54,7 @@ class HealthIcon extends FlxSprite
 					animation.add(char, [0], 0, false, isPlayer);
 					animation.play(char);
 					this.char = char;
+				// 2 icons
 				case 300:
 					loadGraphic(file, true, Math.floor(width / 2), Math.floor(height));
 					iconOffsets[0] = (width - 150) / 2;
@@ -63,6 +64,7 @@ class HealthIcon extends FlxSprite
 					animation.add(char, [0, 1], 0, false, isPlayer);
 					animation.play(char);
 					this.char = char;
+				// 3 icons
 				case 450:
 					loadGraphic(file, true, Math.floor(width / 3), Math.floor(height));
 					iconOffsets[0] = (width - 150) / 3;
@@ -73,6 +75,7 @@ class HealthIcon extends FlxSprite
 					animation.add(char, [0, 1, 2], 0, false, isPlayer);
 					animation.play(char);
 					this.char = char;
+				// 5 icons
 				case 750:
 					loadGraphic(file, true, Math.floor(width / 5), Math.floor(height));
 					iconOffsets[0] = (width - 150) / 5;
@@ -101,7 +104,7 @@ class HealthIcon extends FlxSprite
 		offset.y = iconOffsets[1];
 	}
 
-	public function getCharacter():String {
+	inline public function getCharacter():String {
 		return char;
 	}
 }
