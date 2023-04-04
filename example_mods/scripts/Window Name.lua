8f4d5c0c6463f426c9b3a1c3bca6cf8bb7370dff
Joalor64 <rhgarcon@gmail.com>
2023-04-04 13:53:24 -0400

    probably gonna release today

--- example_mods/scripts/Window Name.lua
+++ example_mods/scripts/Window Name.lua
@@ -1,26 +1,6 @@
-local difficulties = {
-    "Easy",
-    "Normal",
-    "Hard",
-    "Harder",
-    "Ow my hands",
-}
-
-local oldTitle = "Friday Night Funkin: Joalor64 Engine Rewritten"
-
 function onCreate()
-    oldTitle = (getPropertyFromClass("openfl.Lib", "application.window.title") or "Friday Night Funkin': Joalor64 Engine Rewritten")
-    setPropertyFromClass("openfl.Lib", "application.window.title", oldTitle .. " - NOW PLAYING: " .. (songName))
+	setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': Joalor64 Engine Rewritten - NOW PLAYING: " .. (songName))
 end
-
 function onDestroy()
-    setPropertyFromClass("openfl.Lib", "application.window.title", oldTitle)
+	setPropertyFromClass("openfl.Lib", "application.window.title", "Friday Night Funkin': Joalor64 Engine Rewritten")
 end
-
-function onCreatePost()
-    setPropertyFromClass("openfl.Lib", "application.window.title", oldTitle .. " - NOW PLAYING: " .. (songName) .. " on " .. difficulties[difficulty])
-end
-
--- Credits
--- Original by P00P36#7620
--- Improved by Superpowers04#3887
