a9cda47ced5a47b19af973a505d5f429869d5c4e
Joalor64 <rhgarcon@gmail.com>
2023-04-12 11:20:53 -0400

    blahhhh

--- source/meta/data/Achievements.hx
+++ source/meta/data/Achievements.hx
@@ -348,7 +348,7 @@ class AchievementObject extends FlxSpriteGroup {
 		add(achievementText);
 		add(achievementIcon);
 
-		var cam:Array<FlxCamera> = FlxCamera.defaultCameras;
+		var cam:Array<FlxCamera> = FlxG.cameras.setDefaultDrawTarget;
 		if(camera != null) {
 			cam = [camera];
 		}
