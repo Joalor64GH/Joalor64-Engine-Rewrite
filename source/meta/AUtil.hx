1975ab3f65bc894fdcf50762a02ecdd50ef7fbcf
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-04-21 11:07:13 -0400

    80082
    shortened autil and other things

--- source/meta/AUtil.hx
+++ source/meta/AUtil.hx
@@ -6,15 +6,10 @@ using StringTools;
 
 class AUtil
 {
-	public static function txtSplit(path:String)
+	inline public static function txtSplit(path:String)
 	{
-		var tempArray:Array<String> = [];
-
-		tempArray = Assets.getText(path).trim().split('\n');
-		for (i in 0...tempArray.length)
-		{
-			tempArray[i] = tempArray[i].trim();
-		}
-		return tempArray;
+		return [
+			for (i in Assets.getText(path).trim().split('\n')) i.trim()
+		];
 	}
 }
