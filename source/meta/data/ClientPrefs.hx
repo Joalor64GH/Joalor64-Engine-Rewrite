3d834f2d1823b5b50ff8cb23bc4e07ad003f44df
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-05-04 02:40:49 -0400

    k sorry @Joalor64GH I'm back

--- source/meta/data/ClientPrefs.hx
+++ source/meta/data/ClientPrefs.hx
@@ -295,12 +295,13 @@ class ClientPrefs {
 	public static function reloadControls() {
 		PlayerSettings.player1.controls.setKeyboardScheme(KeyboardScheme.Solo);
 
+		/*
 		TitleState.muteKeys = copyKey(keyBinds.get('volume_mute'));
 		TitleState.volumeDownKeys = copyKey(keyBinds.get('volume_down'));
 		TitleState.volumeUpKeys = copyKey(keyBinds.get('volume_up'));
 		FlxG.sound.muteKeys = TitleState.muteKeys;
 		FlxG.sound.volumeDownKeys = TitleState.volumeDownKeys;
-		FlxG.sound.volumeUpKeys = TitleState.volumeUpKeys;
+		FlxG.sound.volumeUpKeys = TitleState.volumeUpKeys;*/
 	}
 	public static function copyKey(arrayToCopy:Array<FlxKey>):Array<FlxKey> {
 		var copiedArray:Array<FlxKey> = arrayToCopy.copy();
