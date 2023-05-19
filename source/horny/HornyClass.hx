338b83b2c9be69022fee170d11a0dd624b183090
Joalor64 <rhgarcon@gmail.com>
2023-05-19 07:19:19 -0400

    Update HornyClass.hx

--- source/horny/HornyClass.hx
+++ source/horny/HornyClass.hx
@@ -1,15 +1,14 @@
 package horny;
 
 class HornyClass {
-	
-	public var script:HornyScript;
+    public var script:HornyScript;
 
     public dynamic function new(path:String, args:Array<Dynamic>) {
         script = new HornyScript(path);
-		script.setVariable("class", this);
-                script.run();
-                script.executeFunc("new", args);
-		script.executeFunc("create", args);
+	script.setVariable("class", this);
+        script.run();
+        script.executeFunc("new", args);
+	script.executeFunc("create", args);
     }
 
     public function set(name:String, val:Dynamic) {
