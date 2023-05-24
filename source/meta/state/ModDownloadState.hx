13244cfee357d4a6119ffc4b4f7ac375c38237d4
Joalor64 <rhgarcon@gmail.com>
2023-05-24 08:30:04 -0400

    in the beninging

--- source/meta/state/ModDownloadState.hx
+++ source/meta/state/ModDownloadState.hx
@@ -306,7 +306,8 @@ class ModDownloadState extends MusicBeatState
 					if (FileSystem.exists('mods/$folder'))
 					{
 						if (FileSystem.readDirectory('mods/$folder/') != null
-							&& !FileSystem.readDirectory('mods/$folder/').contains('#if FUTURE_POLYMOD _polymod_meta.json #else pack.json #end'))
+							&& !FileSystem.readDirectory('mods/$folder/').contains(#if FUTURE_POLYMOD '_polymod_meta.json' #else 'pack.json' #end)
+							&& !FileSystem.readDirectory('mods/$folder/').contains(#if FUTURE_POLYMOD '_polymod_icon.png' #else 'pack.png' #end))
 						{
 							coolText.text = 'This Is Not A Joalor64 Engine Mod!';
 
