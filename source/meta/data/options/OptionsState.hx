df299b38af9500d2165dc39c74b4ccf85b9915a3
Joalor64 <rhgarcon@gmail.com>
2023-05-25 09:40:47 -0400

    Update OptionsState.hx

--- source/meta/data/options/OptionsState.hx
+++ source/meta/data/options/OptionsState.hx
@@ -55,7 +55,7 @@ class OptionsState extends MusicBeatState
 		switch(label) {
 			#if (MODS_ALLOWED && FUTURE_POLYMOD)
 			case 'Mod Options':
-			    if (Paths.optionsExist())
+			    	if (Paths.optionsExist())
 					FlxG.switchState(new ModOptionSelectState());
 				else
 					FlxG.switchState(new OopsState());
