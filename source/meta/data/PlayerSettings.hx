7d271d8c12f516d5a43bb6728334bbbf4e4de84e
Hoovy <124418090+MemeHovy@users.noreply.github.com>
2023-06-15 03:55:53 -0400

    cleaned this up

--- source/meta/data/PlayerSettings.hx
+++ source/meta/data/PlayerSettings.hx
@@ -2,40 +2,25 @@ package meta.data;
 
 import meta.Controls;
 import flixel.FlxG;
-import flixel.util.FlxSignal;
 
 class PlayerSettings
 {
-	static public var numPlayers(default, null) = 0;
-	static public var numAvatars(default, null) = 0;
 	static public var player1(default, null):PlayerSettings;
-	static public var player2(default, null):PlayerSettings;
-
-	static public final onAvatarAdd = new FlxTypedSignal<PlayerSettings->Void>();
-	static public final onAvatarRemove = new FlxTypedSignal<PlayerSettings->Void>();
 
 	public var id(default, null):Int;
 
 	public final controls:Controls;
 
-	function new(id, scheme)
-	{
-		this.id = id;
-		this.controls = new Controls('player$id', scheme);
-	}
+	function new(scheme)
+		this.controls = new Controls('player0', scheme);
 
 	public function setKeyboardScheme(scheme)
-	{
 		controls.setKeyboardScheme(scheme);
-	}
 
 	static public function init():Void
 	{
 		if (player1 == null)
-		{
 			player1 = new PlayerSettings(0, Solo);
-			++numPlayers;
-		}
 
 		var numGamepads = FlxG.gamepads.numActiveGamepads;
 		if (numGamepads > 0)
@@ -46,27 +31,8 @@ class PlayerSettings
 
 			player1.controls.addDefaultGamepad(0);
 		}
-
-		if (numGamepads > 1)
-		{
-			if (player2 == null)
-			{
-				player2 = new PlayerSettings(1, None);
-				++numPlayers;
-			}
-
-			var gamepad = FlxG.gamepads.getByID(1);
-			if (gamepad == null)
-				throw 'Unexpected null gamepad. id:0';
-
-			player2.controls.addDefaultGamepad(1);
-		}
 	}
 
-	static public function reset()
-	{
+	static public inline function reset()
 		player1 = null;
-		player2 = null;
-		numPlayers = 0;
-	}
 }
