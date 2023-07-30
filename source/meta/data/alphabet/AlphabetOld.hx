fc622087451d84cfded70eb7b926ec31ff96e1b0
Joalor64 <rhgarcon@gmail.com>
2023-07-29 23:41:19 -0400

    now im tired gn

--- source/meta/data/alphabet/AlphabetOld.hx
+++ source/meta/data/alphabet/AlphabetOld.hx
@@ -454,7 +454,7 @@ class AlphaCharacter extends FlxSprite
 	public function new(x:Float, y:Float, textSize:Float)
 	{
 		super(x, y);
-		var tex = Paths.getSparrowAtlas('UI/alphabetOld');
+		var tex = Paths.getSparrowAtlas('alphabetOld');
 		frames = tex;
 
 		setGraphicSize(Std.int(width * textSize));
