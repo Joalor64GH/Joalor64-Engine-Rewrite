8b718d93e533c930abb37c43fe95fa480fc2d5f1
Joalor64 <rhgarcon@gmail.com>
2023-08-06 19:16:27 -0400

    yoo new wip startup just dropped

--- source/Init.hx
+++ source/Init.hx
@@ -3,11 +3,15 @@ package;
 #if desktop
 import meta.data.dependency.Discord.DiscordClient;
 #end
+
 import flixel.FlxG;
 import flixel.FlxState;
 import flixel.FlxSprite;
 import flixel.text.FlxText;
 import flixel.util.FlxTimer;
+import flixel.util.FlxColor;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
 import flixel.input.keyboard.FlxKey;
 import lime.app.Application;
 import haxe.Http;
@@ -15,23 +19,26 @@ import haxe.Http;
 import meta.*;
 import meta.state.*;
 import meta.data.*;
-import meta.state.PlayState;
+
 #if FUTURE_POLYMOD
 import core.ModCore;
 #end
 
-class Init extends FlxState // this loads everything in
+// this loads everything in
+class Init extends FlxState
 {
-	public static var randomIcon:Array<String> = [
-		'joalor', 
-		'meme', 
-		'fox', 
-		'bot'
-	];
+	public static var randomIcon:Array<String> = ['joalor', 'meme', 'fox', 'bot']; // WILL BE REPLACED LATER!!!
+	public static var updateVersion:String = '';
+
+	var loadingSpeen:FlxSprite;
 	var epicSprite:FlxSprite;
 
-	public static var updateVersion:String = '';
+	var coolText:FlxText;
+
     	var mustUpdate:Bool = false;
+	var isTweening:Bool = false;
+
+	var lastString:String = '';
 
 	public function new() 
 	{
@@ -44,8 +51,8 @@ class Init extends FlxState // this loads everything in
 		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
-        	bg.scale.set(10, 10);
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image("loader/bgDesat"));
+		bg.color = randomizeColor();
 		bg.screenCenter();
         	add(bg);
         
@@ -55,10 +62,19 @@ class Init extends FlxState // this loads everything in
 		epicSprite.screenCenter();
         	add(epicSprite);
 
-		var coolText:FlxText = new FlxText(5, FlxG.height - 24, 0, "Loading...", 22);
+		var bottomPanel:FlxSprite = new FlxSprite(0, FlxG.height - 100).makeGraphic(FlxG.width, 100, 0xFF000000);
+		bottomPanel.alpha = 0.5;
+		add(bottomPanel);
+
+		coolText = new FlxText(20, FlxG.height - 80, 1000, "", 22);
 		coolText.scrollFactor.set();
-		coolText.setFormat("VCR OSD Mono", 26, 0xFFffffff, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		coolText.setFormat("VCR OSD Mono", 26, 0xFFffffff, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(coolText);
+
+		loadingSpeen = new FlxSprite(FlxG.width - 91 ,FlxG.height - 91).loadGraphic(Paths.image("loader/loader"));
+		loadingSpeen.angularVelocity = 180;
+		loadingSpeen.antialiasing = ClientPrefs.globalAntialiasing;
+		add(loadingSpeen);
 
 		FlxG.sound.play(Paths.sound('credits/goofyahhphone'));
 
@@ -69,13 +85,33 @@ class Init extends FlxState // this loads everything in
 			startGame();
 		});
 
+		FlxG.camera.fade(FlxColor.BLACK, 0.33, true);
+
         	super.create();
     	}
 
+	var selectedSomethin:Bool = false;
+	var timer:Float = 0;
+
 	override function update(elapsed)
 	{
-		if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.SPACE)
-			skip();
+		if (!selectedSomethin) {
+			if (isTweening) {
+				coolText.screenCenter(X);
+				timer = 0;
+			} else {
+				coolText.screenCenter(X);
+				timer += elapsed;
+				if (timer >= 3)
+				{
+					changeText();
+				}
+			}
+		}
+
+		if (FlxG.keys.justPressed.ENTER || FlxG.keys.justPressed.SPACE) {
+			startGame(); // in case you wanna skip
+		}
 
 		super.update(elapsed);
 	}
@@ -155,9 +191,40 @@ class Init extends FlxState // this loads everything in
 		Highscore.load();
 	}
 
-	function skip() 
+	function changeText()
 	{
-		startGame();
+		var selectedText:String = '';
+		var textArray:Array<String> = CoolUtil.coolTextFile(Paths.txt('tipText')); // basically introText.txt
+
+		coolText.alpha = 1;
+		isTweening = true;
+		selectedText = textArray[FlxG.random.int(0, (textArray.length - 1))].replace('--', '\n');
+		FlxTween.tween(coolText, {alpha: 0}, 1, {
+			ease: FlxEase.linear,
+			onComplete: function(shit:FlxTween)
+			{
+				if (selectedText != lastString)
+				{
+					coolText.text = selectedText;
+					lastString = selectedText;
+				}
+				else
+				{
+					selectedText = textArray[FlxG.random.int(0, (textArray.length - 1))].replace('--', '\n');
+					coolText.text = selectedText;
+				}
+
+				coolText.alpha = 0;
+
+				FlxTween.tween(coolText, {alpha: 1}, 1, {
+					ease: FlxEase.linear,
+					onComplete: function(shit:FlxTween)
+					{
+						isTweening = false;
+					}
+				});
+			}
+		});
 	}
 
 	function startGame() 
@@ -183,4 +250,11 @@ class Init extends FlxState // this loads everything in
 		var chance:Int = FlxG.random.int(0, randomIcon.length - 1);
 		return Paths.image('credits/${randomIcon[chance]}');
 	}
+
+		public static function randomizeColor()
+    	{
+		var chance:Int = FlxG.random.int(0, coolColors.length - 1);
+		var color:FlxColor = coolColors[chance];
+		return color;
+   	}
 }
