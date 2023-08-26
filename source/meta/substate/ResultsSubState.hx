8edaf3cf2e57119f830b5e31ebb6e1a948fb3792
Joalor64 <rhgarcon@gmail.com>
2023-08-25 20:50:41 -0400

    Update ResultsSubState.hx

--- source/meta/substate/ResultsSubState.hx
+++ source/meta/substate/ResultsSubState.hx
@@ -28,28 +28,28 @@ class ResultsSubState extends MusicBeatSubState {
         bg.alpha = 0.4;
         add(bg);
 
-        var titleTxt:FlxText = new FlxText(5, 0, 0, (PauseSubState.daSelected == 'SKip Song') ? 'y u skip??' : 'RESULTS', 72);
-		titleTxt.scrollFactor.set();
-		titleTxt.setFormat("VCR OSD Mono", 48, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		titleTxt.updateHitbox();
-		add(titleTxt);
+        var titleTxt:FlxText = new FlxText(5, 0, 0, (PauseSubState.daSelected == 'Skip Song') ? 'y u skip??' : 'RESULTS', 72);
+	titleTxt.scrollFactor.set();
+	titleTxt.setFormat("VCR OSD Mono", 48, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+	titleTxt.updateHitbox();
+	add(titleTxt);
     }
 
     override function update(elapsed:Float) {
-		super.update(elapsed);
+	super.update(elapsed);
 
-		if (controls.ACCEPT) {
-			if (PlayState.isStoryMode)
-				MusicBeatState.switchState(new StoryMenuState());
-			else {
-				if (PlayState.inMini) {
-					inMini = false;
-					MusicBeatState.switchState(new MinigamesState());
-				} else {
-					MusicBeatState.switchState(new FreeplayState());
-				}
+	if (controls.ACCEPT) {
+	    if (PlayState.isStoryMode)
+		MusicBeatState.switchState(new StoryMenuState());
+	    else {
+		if (PlayState.inMini) {
+		    inMini = false;
+		    MusicBeatState.switchState(new MinigamesState());
+		} else {
+		    MusicBeatState.switchState(new FreeplayState());
+		}
             }
             FlxG.sound.playMusic(Paths.music('freakyMenu'));
-		}
 	}
+    }
 }
