71e6db5e2c0d4b2367eefa37ea237d55acfa2efa
✞Hoovy✞ <124418090+MemeHovy@users.noreply.github.com>
2023-09-03 05:09:55 -0400

    codespaces aren’t working

--- source/github/APIShit.hx
+++ source/github/APIShit.hx
@@ -4,7 +4,7 @@ class APIShit
 {
     public static var repoHolder:String = 'Joalor64GH';
     public static var repoName:String = 'Joalor64-Engine-Rewrite';
-    public static var personalAccessToken:String = ''; // unnecessary thingy i guess
+    public static var personalAccessToken:String = ''; // used for private repos
 }
  // CHANGE THIS IF YOU ARE MAKING A MOD!!!!!!!!!!!!!!!!!!
   // CHANGE THIS IF YOU ARE MAKING A MOD!!!!!!!!!!!!!!!!!!
