1936d8b4fdebe30a466cc92bf9e19ef291a2e3b6
Joalor64 <rhgarcon@gmail.com>
2023-11-28 09:38:51 -0500

    i mean okay then

--- source/meta/data/scripts/FunkinSScript.hx
+++ source/meta/data/scripts/FunkinSScript.hx
@@ -32,9 +32,9 @@ import objects.*;
 
 final class FunkinSScript extends tea.SScript
 {
-	public function new(?scriptFile:String = "", ?preset:Bool = true, ?startExecute:Bool = true)
+	public function new(?scriptFile:String = "", ?preset:Bool = true)
 	{
-		super(scriptFile, preset, false);
+		super(scriptFile, preset;
 		
 		execute();
 	}
