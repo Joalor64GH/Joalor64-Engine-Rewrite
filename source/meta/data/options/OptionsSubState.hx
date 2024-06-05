82ab14e89f9a2479f1b6219d64adced3bebf282e
Joalor64 <rhgarcon@gmail.com>
2024-06-05 07:53:56 -0400

    Update OptionsSubState.hx

--- source/meta/data/options/OptionsSubState.hx
+++ source/meta/data/options/OptionsSubState.hx
@@ -686,7 +686,7 @@ class GameplaySubState extends BaseOptionsMenu
 			'bool',
 			true);
 		addOption(option);
-		option.onchange = onChangeAutoPause;
+		option.onChange = onChangeAutoPause;
 
 		var option:Option = new Option('Disable Reset Button',
 			"If checked, pressing Reset won't do anything.",
