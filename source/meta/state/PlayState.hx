831274c59b5cc017f0cc2200fcf00c31a16d1666
Joalor64 <rhgarcon@gmail.com>
2024-06-06 09:21:08 -0400

    Update PlayState.hx

--- source/meta/state/PlayState.hx
+++ source/meta/state/PlayState.hx
@@ -244,7 +244,7 @@ class PlayState extends MusicBeatState
 	var gunsNoteTweens:Array<FlxTween> = [];
 	// the funny thing is, i am considering adding extra notes in the future
 	// but idk if that's possible with the fact that hsv note coloring was removed
-	public static var (default, set):Int = 3;
+	public static var mania(default, set):Int = 3;
 	public static function set_mania(newMania:Int) {
 		mania = newMania;
 		if (PlayState.instance == null) return mania;
