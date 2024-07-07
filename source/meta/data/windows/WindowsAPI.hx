ab9bf4d2674aa2df3b71dd759a66eb3692feb6d1
Joalor64 <rhgarcon@gmail.com>
2024-07-07 17:10:34 -0400

    Update WindowsAPI.hx

--- source/meta/data/windows/WindowsAPI.hx
+++ source/meta/data/windows/WindowsAPI.hx
@@ -45,31 +45,6 @@ class WindowsAPI
     {
         showMessageBox(caption, message, icon);
     }
-
-    @:functionCode('
-    HWND window = getActiveWindow();
-    ')
-    public static function getHWNDWindow(){
-        return null;
-    }
-
-    @:functionCode('
-    unsigned long long dedicatedRAM = 0;
-    GetPhysicallyInstalledSystemMemory(dedicatedRAM);
-    return dedicatedRAM / 1024;
-    ')
-    public static function getRAM():UInt64 {
-        return 0;
-    }
-
-    @:functionCode('
-    srand((unsigned) time(NULL));
-    int random = rand();
-    return 1;
-    ')
-    public static function randomNumber():UInt64 {
-        return 1;
-    }
 }
 
 @:enum abstract MessageBoxIcon(Int) {
